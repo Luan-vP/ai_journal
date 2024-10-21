db067dd9d4d60f7967de6c6f87448d84d25e9309
Luan van Pletsen <luanvanpletsen@gmail.com>
2024-10-21 17:40:52 +0100

    test: extend test cases for text upload to collection

--- backend/test/test_storage.py
+++ backend/test/test_storage.py
@@ -6,27 +6,31 @@ from conftest import test_weaviate_client
 
 def test_upload_text_to_weaviate_collection(test_weaviate_client):
 
-    # TODO
-    # test no matches for vector search on "mango"
     collection = test_weaviate_client.collections.get(WEAVIATE_COLLECTION_NAME)
     response = collection.query.near_text(
-        query="animals in movies",
-        limit=1,
+        query="any random query",
+        limit=10,
         # return_metadata=MetadataQuery(distance=True)
     )
 
     assert len(response.objects) == 0
-    
 
     upload_text_to_weaviate_collection("mango is my favourite fruit", test_weaviate_client)
 
-    # TODO
-    # Test there is now a match for "mango"
-
     response = collection.query.near_text(
-        query="animals in movies",
-        limit=1,
+        query="any random query",
+        limit=10,
         # return_metadata=MetadataQuery(distance=True)
     )
 
     assert len(response.objects) == 1
+
+    upload_text_to_weaviate_collection("pineapple is my new favourite fruit!", test_weaviate_client)
+
+    response = collection.query.near_text(
+        query="any random query",
+        limit=10,
+        # return_metadata=MetadataQuery(distance=True)
+    )
+
+    assert len(response.objects) == 2
