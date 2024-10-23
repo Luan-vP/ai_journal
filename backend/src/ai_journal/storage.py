4b794a630b541a08c3e7b37eedbbeee3e7507cde
Luan van Pletsen <luanvanpletsen@gmail.com>
2024-10-23 20:46:41 +0100

    test: add testing of existing functionality

--- backend/src/ai_journal/storage.py
+++ backend/src/ai_journal/storage.py
@@ -1,7 +1,19 @@
+import contextlib
+import os
+import uuid
 from pathlib import Path
 
+import weaviate
+from weaviate import WeaviateClient
+import weaviate.classes.config as wvcc
+from weaviate.classes.config import Configure
 
-user_data_location = Path(__file__).resolve().parent.parent / "user_data"
+WEAVIATE_COLLECTION_NAME = (
+    os.getenv("WEAVIATE_COLLECTION_NAME") or "WeaviateJournalChunks"
+)
+WEAVIATE_HOST = os.getenv("WEAVIATE_HOST") or "localhost"
+
+user_data_location = Path(__file__).resolve().parents[2] / "local_data"
 print(user_data_location)
 user_data_location.mkdir(exist_ok=True)
 
@@ -27,3 +39,44 @@ def write_to_new_file(content: str):
     with open(new_file_path, "w") as file:
         file.write(content)
     return new_file_name
+
+
+@contextlib.contextmanager
+def get_weaviate_client():
+    client = weaviate.connect_to_local(host=WEAVIATE_HOST)
+
+    try:
+        yield client
+    finally:
+        client.close()
+
+
+def create_default_collection(weaviate_client: WeaviateClient):
+    weaviate_client.collections.create(
+        name=WEAVIATE_COLLECTION_NAME,
+        vectorizer_config=[
+            Configure.NamedVectors.text2vec_ollama(
+                name="title_vector",
+                source_properties=["title"],
+                api_endpoint="http://localhost:11434",  # TODO switch this to docker with a startup scripts
+                model="llama3:8b",
+            )
+        ],
+        generative_config=Configure.Generative.ollama(
+            api_endpoint="http://localhost:11434"
+        ),
+        properties=[
+            wvcc.Property(name="content", data_type=wvcc.DataType.TEXT),
+            wvcc.Property(name="author", data_type=wvcc.DataType.TEXT),
+        ],
+    )
+
+
+def upload_text_to_weaviate_collection(
+    text: str, weaviate_client: weaviate.WeaviateClient
+) -> uuid.UUID:
+    collection = weaviate_client.collections.get(WEAVIATE_COLLECTION_NAME)
+
+    uuid = collection.data.insert({"text": text})
+
+    return uuid
---
+++ backend/test/test_storage.py
@@ -0,0 +1,36 @@
+from ai_journal.storage import (
+    upload_text_to_weaviate_collection,
+    WEAVIATE_COLLECTION_NAME,
+)
+
+
+def test_upload_text_to_weaviate_collection(test_weaviate_client):
+    collection = test_weaviate_client.collections.get(WEAVIATE_COLLECTION_NAME)
+    response = collection.query.near_text(
+        query="any random query",
+        limit=10,
+    )
+
+    assert len(response.objects) == 0
+
+    upload_text_to_weaviate_collection(
+        "mango is my favourite fruit", test_weaviate_client
+    )
+
+    response = collection.query.near_text(
+        query="any random query",
+        limit=10,
+    )
+
+    assert len(response.objects) == 1
+
+    upload_text_to_weaviate_collection(
+        "pineapple is my new favourite fruit!", test_weaviate_client
+    )
+
+    response = collection.query.near_text(
+        query="any random query",
+        limit=10,
+    )
+
+    assert len(response.objects) == 2
